088eeec54c59a8e4c717fd2cb3cb36bdff024458
Alessandro Tecchio <122892593+suppa96@users.noreply.github.com>
2024-03-01 16:49:49 +0100

    feat: initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,5 @@
+<template>
+  <div style="background-color: red">Header</div>
+  <slot />
+  <div style="background-color: blue">Footer</div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="h-full">I'm a funny homepage setup</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.h-full {
+  height: 100%;
+}
+</style>
