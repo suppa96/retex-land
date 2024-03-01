700a4266140295a54357c5477a4e3913d13c0173
Alessandro Tecchio <122892593+suppa96@users.noreply.github.com>
2024-03-01 17:44:29 +0100

    style: fix page size

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="h-screen w-screen m-0 p-0">
+  <main class="h-screen w-screen m-0 p-0 flex flex-col justify-between">
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
