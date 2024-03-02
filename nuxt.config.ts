259f3e71dedd571f653ea09d6a81ad099ffcd218
Alessandro Tecchio <122892593+suppa96@users.noreply.github.com>
2024-03-02 06:13:02 +0100

    chore: remove fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
-import IconsResolver from 'unplugin-icons/resolver'
-import Icons from 'unplugin-icons/vite'
-import Components from 'unplugin-vue-components/vite'
-import { FileSystemIconLoader } from 'unplugin-icons/loaders'
-import { fileURLToPath } from 'url'
+import IconsResolver from "unplugin-icons/resolver";
+import Icons from "unplugin-icons/vite";
+import Components from "unplugin-vue-components/vite";
+import { FileSystemIconLoader } from "unplugin-icons/loaders";
+import { fileURLToPath } from "url";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
@@ -10,13 +10,13 @@ export default defineNuxtConfig({
     typeCheck: true,
   },
   devtools: { enabled: true },
-  modules: ['@pinia/nuxt', '@nuxtjs/google-fonts', "@nuxt/image"],
+  modules: ["@pinia/nuxt", "@nuxt/image"],
   css: ["~/assets/css/main.scss"],
   googleFonts: {
     families: {
       Poppins: [300, 400, 500, 700],
     },
-    display: 'swap'
+    display: "swap",
   },
   vite: {
     plugins: [
@@ -24,7 +24,7 @@ export default defineNuxtConfig({
         resolvers: [
           IconsResolver({
             prefix: false,
-            customCollections: ['icons'],
+            customCollections: ["icons"],
           }),
         ],
         dts: true,
@@ -32,15 +32,15 @@ export default defineNuxtConfig({
       Icons({
         customCollections: {
           icons: FileSystemIconLoader(
-            fileURLToPath(new URL('./icons/', import.meta.url)),
-            svg => {
-              const viewBox = /viewBox="\d+ \d+ (\d+) (\d+)"/gi.exec(svg)
-              if (!viewBox) return svg
-              const w = viewBox?.[1]
-              const h = viewBox?.[2]
-              svg = svg.replace(/width=".*?"/, `width="${w}" `)
-              svg = svg.replace(/height=".*?"/, `height="${h}" `)
-              return svg
+            fileURLToPath(new URL("./icons/", import.meta.url)),
+            (svg) => {
+              const viewBox = /viewBox="\d+ \d+ (\d+) (\d+)"/gi.exec(svg);
+              if (!viewBox) return svg;
+              const w = viewBox?.[1];
+              const h = viewBox?.[2];
+              svg = svg.replace(/width=".*?"/, `width="${w}" `);
+              svg = svg.replace(/height=".*?"/, `height="${h}" `);
+              return svg;
             }
           ),
         },
